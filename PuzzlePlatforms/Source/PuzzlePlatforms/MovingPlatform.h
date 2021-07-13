c95f768da0885e5f0cfafcb695aba4897feaa313
Aaron Watkins <ac.watkins80@gmail.com>
2021-07-12 21:50:18 -0400

    add moving platform c++ class move the platform on the server but not on the client

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere)
+	float SpeedInXDir = 20;
+	
+public:
+	AMovingPlatform();
+
+	virtual void Tick(float DeltaTime) override;
+};
