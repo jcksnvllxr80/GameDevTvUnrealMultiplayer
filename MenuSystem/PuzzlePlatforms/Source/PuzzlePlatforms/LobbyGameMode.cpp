5d0361a4371582abe5a6d94e6d24fd440676196c
Aaron Watkins <ac.watkins80@gmail.com>
2021-09-02 23:45:18 -0400

    add transition map and use seamless travel to load into game from lobby

--- MenuSystem/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ MenuSystem/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -5,11 +5,22 @@
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
+	Super::PostLogin(NewPlayer);
 	++NumberOfPlayers;
 	UE_LOG(LogTemp, Warning, TEXT("There are %i players in the lobby."), NumberOfPlayers);
+	if(NumberOfPlayers >= 3)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Leaving lobby to start game..."), NumberOfPlayers);
+		UWorld* World = GetWorld();
+		if (!World) return;
+		bUseSeamlessTravel = true;
+		World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+	}
 }
 
 void ALobbyGameMode::Logout(AController* Exiting)
 {
+	Super::Logout(Exiting);
 	--NumberOfPlayers;
+	UE_LOG(LogTemp, Warning, TEXT("There are %i players in the lobby."), NumberOfPlayers);
 }
